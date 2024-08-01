8f5af79feb31f8dda97385e645282e51afa1c3d3
jackretterer <jack.retterer@gmail.com>
2024-08-01 16:27:09 -0700

    trying to fix height?

--- components/ui/wavy-background.tsx
+++ components/ui/wavy-background.tsx
@@ -50,12 +50,12 @@ export const WavyBackground = ({
     canvas = canvasRef.current;
     ctx = canvas.getContext("2d");
     w = ctx.canvas.width = window.innerWidth;
-    h = ctx.canvas.height = window.innerHeight;
+    h = ctx.canvas.height = window.innerHeight * 0.9;
     ctx.filter = `blur(${blur}px)`;
     nt = 0;
     window.onresize = function () {
       w = ctx.canvas.width = window.innerWidth;
-      h = ctx.canvas.height = window.innerHeight;
+      h = ctx.canvas.height = window.innerHeight * 0.9;
       ctx.filter = `blur(${blur}px)`;
     };
     render();
